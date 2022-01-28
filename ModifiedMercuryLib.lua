b975ba92f2a7d6b89584b8c7399029dd047d618b
SirZyxx <95705985+SirZyxx@users.noreply.github.com>
2022-01-28 13:12:28 -0700

    Update ModifiedMercuryLib.lua

--- ModifiedMercuryLib.lua
+++ ModifiedMercuryLib.lua
@@ -1780,11 +1780,16 @@ end
 
 function Library:label(options)
 	options = self:set_defaults({
-		Name = "Label",
-		Text = "Textt"
+		Name = "Button",
+		Text = "Text"
 	}, options)
 
-	local text = self.container:object("TextLabel", {
+	local buttonContainer = self.container:object("TextButton", {
+		Theme = {BackgroundColor3 = "Secondary"},
+		Size = UDim2.new(1, -20, 0, 52)
+	}):round(7)
+
+	local text = buttonContainer:object("TextLabel", {
 		BackgroundTransparency = 1,
 		Position = UDim2.fromOffset(10, (options.Description and 5) or 0),
 		Size = (options.Description and UDim2.new(0.5, -10, 0, 22)) or UDim2.new(0.5, -10, 1, 0),
@@ -1794,12 +1799,47 @@ function Library:label(options)
 		TextXAlignment = Enum.TextXAlignment.Left
 	})
 
-	
+
+	local icon = buttonContainer:object("ImageLabel", {
+		AnchorPoint = Vector2.new(1, 0.5),
+		BackgroundTransparency = 1,
+		Position = UDim2.new(1, -11, 0.5, 0),
+		Size = UDim2.fromOffset(26, 26),
+		Image = "rbxassetid://8498776661",
+		Theme = {ImageColor3 = "Tertiary"}
+	})
+
+	do
+		local hovered = false
+		local down = false
+
+		buttonContainer.MouseEnter:connect(function()
+			hovered = true
+			--buttonContainer:tween{BackgroundColor3 = self:lighten(Library.CurrentTheme.Secondary, 10)}
+		end)
+
+		buttonContainer.MouseLeave:connect(function()
+			hovered = false
+			if not down then
+				buttonContainer:tween{BackgroundColor3 = Library.CurrentTheme.Secondary}
+			end
+		end)
+
+		buttonContainer.MouseButton1Down:connect(function()
+			--buttonContainer:tween{BackgroundColor3 = self:lighten(Library.CurrentTheme.Secondary, 20)}
+		end)
+
+		UserInputService.InputEnded:connect(function(key)
+			if key.UserInputType == Enum.UserInputType.MouseButton1 then
+				--buttonContainer:tween{BackgroundColor3 = (hovered and self:lighten(Library.CurrentTheme.Secondary)) or Library.CurrentTheme.Secondary}
+			end
+		end)
+	end
 	self:_resize_tab()
 
 	local methods = {}
 
-	function methods:UpdateLabel(UpdatedText)
+	function methods:UpdateText(UpdatedText)
 		options.Text = UpdatedText
 	end
 
