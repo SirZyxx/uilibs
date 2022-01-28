b67bede9e7ee55736e925169ceb19274db0ad17d
SirZyxx <95705985+SirZyxx@users.noreply.github.com>
2022-01-28 13:41:38 -0700

    Update ModifiedMercuryLib.lua

--- ModifiedMercuryLib.lua
+++ ModifiedMercuryLib.lua
@@ -899,6 +899,7 @@ function Library:create(options)
 
 	creditsTab:credit{Name = "Abstract", Description = "UI Library Developer", Discord = "Abstract#8007", V3rmillion = "AbstractPoo"}
 	creditsTab:credit{Name = "Deity", Description = "UI Library Developer", Discord = "Deity#0228", V3rmillion = "0xDEITY"}
+	creditsTab:credit{Name = "Sir Zyx", Description = "Modified Shit", Discord = "sirzyx.#7222", V3rmillion = "SirZyx"}
 
 	return mt
 end
